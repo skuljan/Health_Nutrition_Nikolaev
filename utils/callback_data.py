e1a6038e13f80a10d2b3c781725288eef235f719
skuljan <69480837+skuljan@users.noreply.github.com>
2024-06-16 16:05:07 +0300

    Update callback_data.py

--- utils/callback_data.py
+++ utils/callback_data.py
@@ -3,7 +3,7 @@
 import base64
 
 def encode_callback_data(data):
-    """Убедитесь, что данные обратного вызова не превышают лимит Telegram и содержат только допустимые символы."""
+    """данные обратного вызова не превышают лимит Telegram и содержат только допустимые символы."""
     return base64.urlsafe_b64encode(data.encode('utf-8')).decode('utf-8')[:64]
 
 def decode_callback_data(data):
